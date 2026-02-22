660770b359d34eb764ec02aa2659c78cb78dad8b
jahnavi-gaur <121301783+jahnavi-gaur@users.noreply.github.com>
2026-02-22 10:44:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function findLargest(a, b, c) {
   //your code here
+	let lar
+	if(a >= b && a >= c){
+		lar = a
+	}
+	else if(b >= a && b >= c){
+		lar = b 
+	}
+	else if(c >= a && c >= b){
+		lar = c 
+	}
+	return lar;
 }
 
 const num1 = parseInt(prompt("Enter First Number."));
